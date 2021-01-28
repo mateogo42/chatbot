a143443acf244c28e9667721c8f228a1e27b3bc5
Mateo Gonzalez <magonzalezoc@gmail.com>
2021-01-27 20:27:20 -0500

    Changed API to so that it can be called from facebook messenger

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
 from pydantic import BaseModel
 import pickle
+from typing import List
 from cli import preprocess_input
 from pathlib import Path
 from model import Chatbot
 import numpy as np
+import requests
+import os
+import json
+
+TOKEN = os.environ.get("VERIFY_TOKEN")
 
 with open(Path("data/tokenizer.pkl"), 'rb') as f:
     tokenizer_data = pickle.load(f)
@@ -24,14 +30,40 @@ print("[*] Finished loading model weights")
 
 app = FastAPI()
 
-class Message(BaseModel):
-    text: str
+class WebhookData(BaseModel):
+    object: str = ""
+    entry: List = []
 
 
-@app.post("/chat")
-def chat(message: Message):
+@app.router.get("/")
+async def verify(request: Request):
 
-    question = preprocess_input(message.text) 
+    if request.query_params.get("hub.mode") == "subscribe" and request.query_params.get("hub.challenge"):
+        if not request.query_params.get("hub.verify_token") == TOKEN:
+            return Response(content="Los tokens no coinciden", status_code=403)
+        return Response(content=request.query_params["hub.challenge"])
+
+    return Response(content="Faltan argumentos en el request", status_code=400)
+
+
+
+@app.post("/")
+async def chat(webhook_data: WebhookData):
+
+    if webhook_data.object == 'page':
+        for entry in webhook_data.entry:
+            for event in entry['messaging']:
+                if event.get('message', None):
+                    sender_id = event['sender']['id']
+                    recipient_id = event['recipient']['id']
+                    text = event['message']['text']
+                    answer = create_response(text)
+                    send_message(recipient_id, answer)
+
+    
+
+def create_response(text: str) -> str:
+    question = preprocess_input(text) 
     stop_condition = False
     h, c = model.encoder.predict(question)
     empty_target_seq = np.array([[tokenizer.word_index['start']]])
@@ -47,9 +79,23 @@ def chat(message: Message):
 
         empty_target_seq = np.array([[sampled_word_index]])
 
-    return {
-        'response': answer
-    }
+    return answer
+
+async def send_message(recipient_id: str, message: str):
     
+    params = {'access-token': TOKEN}
+    headers = {'Content-Type': 'application/json'}
+    data = json.dumps({
+        'recipient': {
+            'id': recipient_id
+        },
+        'message': {
+            'text': message
+        }
+    })
+
+    response = requests.post('https://graph.facebook.com/v2.6/me/messages', params=params, headers=headers, data=data)
+
+
 
 
