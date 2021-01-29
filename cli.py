b698a8c6c4dd56bd020ed8e5804f4e574835c48b
Mateo Gonzalez <magonzalezoc@gmail.com>
2021-01-29 01:23:06 -0500

    More refactors on cli.py

--- cli.py
+++ cli.py
@@ -2,27 +2,30 @@ from pathlib import Path
 from model import Chatbot
 from termcolor import colored
 import sys
+import re
 
 
 tokenizer_path = Path("data/tokenizer.pkl")
 weights_path = Path("model/model.hdf5")
 model = Chatbot.from_files(tokenizer_path, weights_path)
+stop_words = ['adios', 'chao', 'gracias']
 
 def chat():
 
     print(colored("MovieBot:", 'red', attrs=['bold']), "Hola, soy un bot programado para responder preguntas sobre peliculas. ¿En qué te puedo ayudar?. Para salir escribe la palabra adios")
-
-    stop_words = ['adios']
     should_exit = False
     try:
         while not should_exit:
-            raw_question = input(">>")
-            should_exit = raw_question.strip() in stop_words
-            if should_exit: break
-            answer = model.chat(raw_question)
-            print(colored("MovieBot:", "red", attrs=['bold']), f"{answer}")
+            text = input(colored("Tu: ", "blue", attrs=['bold']))
+            if any([re.match(w, text.lower()) for w in stop_words]):
+                print(colored("MovieBot:", "red", attrs=['bold']),
+                "Ha sido un placer ayudarte.")
+                should_exit = True
+            else:
+                answer = model.chat(text)
+                print(colored("MovieBot:", "red", attrs=['bold']), f"{answer}")
     except KeyboardInterrupt:
-        pass
+        print("\n")
     except Exception as e:
         print(e)
     finally:
